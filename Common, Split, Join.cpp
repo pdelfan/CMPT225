240a1488122e0780dec8123136f9f35c593e0776
Pouria Delfanazari <pdelfan@gmail.com>
2016-01-18 17:04:20 -0800

    added split

--- Common, Split, Join.cpp
+++ Common, Split, Join.cpp
@@ -27,3 +27,42 @@ CDCatalogue CDCatalogue::Common(const CDCatalogue& cat) const {
     
     return *cat_common;
 }
+
+///////
+
+
+//iterate through two arrays, pass if they are similar. Use flag if they are 
+CDCatalogue CDCatalogue::Split(const CDCatalogue& cat) const {
+    /*CDCatalogue test; return test;*/
+
+    //flag
+    bool flag = true; 
+    CDCatalogue* split_cat = new CDCatalogue;
+
+    for (int i = 0; i < cat.Count(); i++) {
+
+        for (int j = 0; j < numcds; j++) {
+
+            //if the 2 arrays have similar album/artist, pass
+            if (cds[i].GetAlbum() == cat.cds[j].GetAlbum() && cds[i].GetArtist() == cat.cds[j].GetArtist()) {
+
+                flag = false;
+            }
+        }
+
+    //if not similar, add it the the new catalogue "split_cat"
+    if (flag) { split_cat->Insert(CD(cds[i].GetArtist(), cds[i].GetAlbum())); } 
+
+    flag = true; 
+
+    }
+
+    for (int k = 0; k < split_cat->Count(); k++) {
+
+        cout << "This album was only found in cat: \n" << "Artist: " <<split_cat->cds[k].GetArtist() 
+        << "\nAlbum: " << split_cat->cds[k].GetAlbum() << endl;
+    }
+    
+    return *split_cat;     
+
+}
