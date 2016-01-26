583cc5eab952d5685ded783283f78e36d7017aea
Pouria Delfanazari <pdelfan@gmail.com>
2016-01-25 20:45:17 -0800

    so far, it tests InsertFront and InsertBack

--- a2/test/main_test.cpp
+++ a2/test/main_test.cpp
@@ -6,18 +6,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-
 #include "ccqueue.h"
 #include "dlinkedlist.h"
 #include "ticket.h"
 
 using namespace std;
 
-// forward function declarations
 void LLTest();
-void CCQTest();
 
-// program entry point
 int main()
 {
   cout << "Entering DLinkedList test function... " << endl;
@@ -31,6 +27,7 @@ void LLTest()
   // default constructor, InsertFront, InsertBack, ElementAt
   DLinkedList<int> lla;
   lla.InsertFront(5);
+  cout << "-------------------------------------------------\n";
   lla.InsertBack(10);
-  cout << "lla contains " << lla.ElementAt(0) << " at index 0." << endl;
+ //cout << "lla contains " << lla.ElementAt(0) << " at index 0." << endl;
 }
