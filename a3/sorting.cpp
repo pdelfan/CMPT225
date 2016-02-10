cbc5e1d77220ebe514d027ab372ec894b5587706
Josh Vocal <joshvocal@gmail.com>
2016-02-10 14:27:12 -0800

    Update sorting.cpp

--- a3/sorting.cpp
+++ a3/sorting.cpp
@@ -104,11 +104,29 @@ void Merge(T arr[], int low, int mid, int high, int n, int& counter)
 }
 
 // Shell Sort
-// (your comments here)
+// (Start with the largest gap and work down to a gap of 1)
 template <class T>
 int ShellSort(T arr[], int n)
 {
   int count = 0;
+  //Create the largest gap which is floor (arrLength/2) and work down
+  for (int gap = n/2; gap > 0; gap /=2) {
+    //Move up one element and compare with the gap element
+      for (int i = gap; i < n; i++) {
+
+          count++;
+          T temp = arr[i];
+
+          int j;
+          //Switch the element with the gap element if it is greater
+          for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+              //Increase count when array is not in sorted order
+              count++;
+              arr[j] = arr[j - gap];
+          }
+          arr[j] = temp;
+      }
+  }
   
   return count;
 }
