f72266b22585b8b6ada9e3396d7c94e8d848526d
Josh Vocal <joshvocal@gmail.com>
2016-02-28 16:11:52 -0800

    update

--- a4/stocksystem.cpp
+++ a4/stocksystem.cpp
@@ -14,31 +14,45 @@ double balance; // how much money you have in the bank
 
 // default constructor;
 // begin with a balance of $100,000.00
-StockSystem();
+StockSystem() {
+	
+}
 
 // returns the balance member
-double GetBalance();
+double GetBalance() {
+
+}
 
 // Add a new SKU to the system. Do not allow insertion of duplicate sku
-bool StockNewItem(StockItem item);
+bool StockNewItem(StockItem item) {
+
+}
 
 // Locate the item with key itemsku and update its description field.
 // Return false if itemsku is not found.
-bool EditStockItemDescription(int itemsku, string desc);
+bool EditStockItemDescription(int itemsku, string desc) {
+
+}
 
 // Locate the item with key itemsku and update its description field.
 // Return false if itemsku is not found or retailprice is negative.
-bool EditStockItemPrice(int itemsku, double retailprice);
+bool EditStockItemPrice(int itemsku, double retailprice) {
+
+}
 
 // Purchase quantity of item at unitprice each, to reach a maximum (post-purchase) on-hand stock quantity of 1000.
 // Return false if balance is not sufficient to make the purchase,
 //   or if SKU does not exist, or if quantity or unitprice are negative.
 // Otherwise, return true and increase the item's on-hand stock by quantity,
 //   and reduce balance by quantity*unitprice.
-bool Restock(int itemsku, int quantity, double unitprice);
+bool Restock(int itemsku, int quantity, double unitprice) {
+
+}
 
 // Sell an item to a customer, if quantity of stock is available and SKU exists.
 // Reduce stock by quantity, increase balance by quantity*price, and return true if stock available.
 // If partial stock (less than quantity) available, sell the available stock and return true.
 // If no stock, sku does not exist, or quantity is negative, return false.
-bool Sell(int itemsku, int quantity);
+bool Sell(int itemsku, int quantity) {
+
+}
